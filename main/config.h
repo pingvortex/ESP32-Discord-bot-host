e9e6962982a21c89cbb9a1cf3f6d4835f7a0a5f1
PingVortex <nerfshelly123@gmail.com>
2026-01-19 18:21:00 +0100

    Create config.h

---
+++ main/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+ // --- SETTINGS ---
+  const char* token = "URTOKEN"; // YOUR BOT TOKEN
+  String wifiName = "SSID"; // YOUR WIFI NAME/SSID
+  String wifiPassword = "PASSWORD"; // YOUR WIFI PASSWORD
+  String botName = "BOT"; // WHAT NAME DO YOU WANT TO BE DISPLAYED ON THE DISPLAY (Can be left as BOT)
+
+#endif
